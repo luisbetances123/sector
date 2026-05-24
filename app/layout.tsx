066b562beb063d07b3b05415b386403af867ac58
Jeanliz123 <betancesluis@live.com>
2026-05-24 12:44:12 -0400

    fix sidebar white background on scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,7 @@ import AuthGuard from './components/AuthGuard'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      <body className="bg-black antialiased text-white overflow-x-hidden">
-        <AuthGuard>{children}</AuthGuard>
+<body className="bg-zinc-950 antialiased text-white overflow-x-hidden">        <AuthGuard>{children}</AuthGuard>
       </body>
     </html>
   )
