940937f05d72a3321e729ea8b83baea85f75e6b8
Jeanliz123 <betancesluis@live.com>
2026-05-28 11:09:12 -0400

    design: forzar meta viewport en head para agigantar interfaz movil

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es" className="bg-zinc-950" style={{ backgroundColor: '#09090b' }}>
       <head>
+        {/* Doble candado para obligar al iPhone a agigantar la pantalla de una vez por todas */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
         <meta name="theme-color" content="#09090b" />
       </head>
       <body className="bg-zinc-950 antialiased text-white overflow-x-hidden" style={{ backgroundColor: '#09090b' }}>
